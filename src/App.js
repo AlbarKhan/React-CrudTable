96135c500f1bad3fd5eaa72f6c9eaa5049f8ce27
unknown <albarkhan60@gmail.com>
2024-01-17 22:16:32 +0530

    edit functionality done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const data = [
   {
@@ -17,29 +17,67 @@ const data = [
 
 export default function App() {
   const [items, setItems] = useState(data);
-
+  const [editCheck, setEditCheck] = useState(false);
+  const [editItem, setEditItem] = useState(null);
+  // Adding Items
   function handleAdditems(item) {
     setItems((items) => [...items, item]);
   }
+
+  // Check if  want To Edit Items
+  function handleEditCheck(item) {
+    setEditCheck(true);
+    setEditItem(item);
+  }
+
+  // Function to Edit The Item
+  function handleEditItem(name, number, email) {
+    // console.log(editItem);
+    setItems((items) =>
+      items.map((item) =>
+        item.id === editItem.id ? { ...item, name, number, email } : item
+      )
+    );
+    setEditCheck(false);
+    setEditItem(null);
+  }
+  // Delete Items
   function handleDeleteItem(id) {
     setItems((items) => items.filter((item) => item.id !== id));
   }
-  function handleEditItem(name, number, email) {}
   return (
     <div className="app">
-      <h2>CRUD TABLE</h2>
-      <Form onAddItem={handleAdditems} />
-      <Table items={items} onDelete={handleDeleteItem} />
+      <h2>CRUD TABLE - REACT</h2>
+      <Form
+        onAddItem={handleAdditems}
+        editCheck={editCheck}
+        onEdit={handleEditItem}
+        editItem={editItem}
+      />
+      <Table
+        items={items}
+        onDelete={handleDeleteItem}
+        onEdit={handleEditCheck}
+      />
     </div>
   );
 }
 
-function Form({ onAddItem }) {
+function Form({ onAddItem, editCheck, onEdit, editItem }) {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
   const [email, setEmail] = useState("");
 
+  useEffect(() => {
+    if (editItem) {
+      setName(editItem.name || "");
+      setNumber(editItem.number || "");
+      setEmail(editItem.email || "");
+    }
+  }, [editItem]);
+
   function handleSubmit(e) {
+    console.log(editCheck);
     e.preventDefault();
     if (!name) {
       alert("Name required");
@@ -54,7 +92,11 @@ function Form({ onAddItem }) {
       return;
     }
     const newObj = { name, number, email, id: Date.now() };
-    onAddItem(newObj);
+    if (editCheck) {
+      onEdit(name, number, email);
+    } else {
+      onAddItem(newObj);
+    }
     setName("");
     setNumber("");
     setEmail("");
@@ -81,15 +123,24 @@ function Form({ onAddItem }) {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       ></input>
-      <button className="submit">Submit</button>
+      <button
+        className="submit"
+        style={
+          editCheck
+            ? { backgroundColor: "#880ED4" }
+            : { backgroundColor: "#51087E" }
+        }
+      >
+        {editCheck ? "Update" : "Submit"}
+      </button>
     </form>
   );
 }
 
-function Table({ items, onDelete }) {
+function Table({ items, onDelete, onEdit }) {
   return (
     <table>
-      <thead style={{ backgroundColor: "grey" }}>
+      <thead style={{ backgroundColor: "#461257", color: "white" }}>
         <tr>
           <th>Name</th>
           <th>Phone</th>
@@ -103,16 +154,18 @@ function Table({ items, onDelete }) {
             className=""
             key={index}
             style={
-              index % 2 == 0
-                ? { backgroundColor: "#002E63" }
-                : { backgroundColor: "#5865f2" }
+              index % 2 === 0
+                ? { backgroundColor: "#880ED4" }
+                : { backgroundColor: "#51087E" }
             }
           >
             <td>{d.name}</td>
             <td>{d.number}</td>
             <td>{d.email}</td>
             <td>
-              <button className="edit">Edit</button>
+              <button className="edit" onClick={() => onEdit(d)}>
+                Edit
+              </button>
             </td>
             <td>
               <button className="delete" onClick={() => onDelete(d.id)}>
